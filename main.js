653ddf08ad7c0cb8e2d22c15c461987631ab88fd
IsaiasC1999 <isaias.romano99@gmail.com>
2023-08-10 23:51:07 -0300

    resuelto xD

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const btnToggle = document.getElementById('menu-toggle');
-const menuNavToggle = document.getElementById('menu-nav-conteiner')
+const menuNavToggle = document.getElementById('menu-nav-conteiner');
 const btnCloseNav = document.getElementById('menu-btn-close');
+const gg = document.getElementById('fondo-opaciti')
+
 
 btnToggle.onclick = () => {
 
   menuNavToggle.classList.toggle('toggle')
+  gg.classList.toggle('active-div')
+  
 
 
 }
@@ -13,7 +17,11 @@ btnCloseNav.onclick = () => {
 
   console.log("Cerrando")
   menuNavToggle.classList.toggle('toggle')
-
+  gg.classList.toggle('active-div')
+  
 }
 
-
+gg.onclick = ()=>{
+  menuNavToggle.classList.toggle('toggle')
+  gg.classList.toggle('active-div')
+}
